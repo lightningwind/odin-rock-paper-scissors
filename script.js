d35a4ecbb7b70f8ebfa7161ced5326bafffd69cd
David Islam <david.islam24@gmail.com>
2022-04-24 14:47:54 -0700

    Implement function computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+/* Randomly returns the string 'Rock', 
+'Paper', or 'Scissors'. */
+function computerPlay() {
+    const num = Math.floor(Math.random() * 3);
+    return (num === 0) ? 'Rock' :
+    (num === 1) ? 'Paper' :
+    'Scissors';
+}
