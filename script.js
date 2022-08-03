5a999e2dfab76b9efecb3ed420897a168cec625f
David Islam <david.islam24@gmail.com>
2022-08-02 23:28:24 -0700

    Create simple UI

--- script.js
+++ script.js
@@ -51,16 +51,50 @@ function playRound(playerSelection, computerSelection) {
 /* Plays a five round game of Rock paper scissors keeping score 
 and returning the winner/loser at the end. */
 function game() {
+    const NUM_ROUNDS = 5; 
     let score = 0;
-    for (let i = 0; i < 5; i++) { // Play five rounds
-        const playerSelection = prompt('Rock, paper, or scissors?');
-        const computerSelection = computerPlay();
-        score += playRound(playerSelection, computerSelection);
-        console.log(displayMessage(playerSelection, computerSelection));
-    }
-    return (score === 0) ? 'It was a tie!' :
-    (score > 0) ? 'You win!' :
-    'You lose!'; 
+
+    // Create 3 button elements 
+    const btn1 = document.createElement('button'); 
+    const btn2 = document.createElement('button');
+    const btn3 = document.createElement('button');
+
+    const msgDiv = document.createElement('div');
+    const scoreDiv = document.createElement('div');
+    const announceDiv = document.createElement('div'); 
+
+    const buttons = [btn1, btn2, btn3];
+
+    // Update text
+    btn1.textContent = 'Rock'; 
+    btn2.textContent = 'Paper';
+    btn3.textContent = 'Scissors';
+    msgDiv.textContent = 'Message: '; 
+    scoreDiv.textContent = `Running score: ${score}`; 
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const playerSelection = e.target.textContent;
+            const computerSelection = computerPlay(); 
+            score += playRound(playerSelection, computerSelection);
+            msgDiv.textContent = 'Message: ' + displayMessage(playerSelection, computerSelection);
+            scoreDiv.textContent = `Running score: ${score}`; 
+            announceDiv.textContent = ''; 
+            if (Math.abs(score) === NUM_ROUNDS) { // check for winner
+                announceDiv.style.color = 'rebeccapurple';
+                announceDiv.textContent = score > 0 ? "You win!" : "You lose!";
+                score = 0;
+            }
+        });
+    })
+
+    // Add them to the DOM
+    document.body.appendChild(btn1);
+    document.body.appendChild(btn2);
+    document.body.appendChild(btn3);
+    document.body.appendChild(msgDiv);
+    document.body.appendChild(scoreDiv);
+    document.body.appendChild(announceDiv);
 }
 
-console.log(game());
+game();
