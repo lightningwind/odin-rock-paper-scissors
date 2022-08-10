ab79e310e95775d9cd822b633f672d294f35d004
David Islam <david.islam24@gmail.com>
2022-08-10 13:46:35 -0700

    Complete game logic

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const choices = ["rock", "paper", "scissors"];
+const winners = [];
+const NUM_ROUNDS = 5;
 
 function game() {
-    playRound();
+    for (let i = 0; i < NUM_ROUNDS; i++) {
+        playRound();
+    }
+    reportWins();
 }
 
 function playRound() {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
 }
 
 function computerChoice() {
@@ -34,66 +40,25 @@ function validateUserInput(choice) {
 
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Tie"; 
+        return "tie"; 
     } else if ( 
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return "Player wins";
+        return "player";
     } else {
-        return "Computer wins";
+        return "computer";
     }
 }
 
-
-/* Plays a five round game of Rock paper scissors keeping score 
-and returning the winner/loser at the end. */
-// function game() {
-//     const NUM_ROUNDS = 5; 
-//     let score = 0;
-
-//     // Create 3 button elements 
-//     const btn1 = document.createElement('button'); 
-//     const btn2 = document.createElement('button');
-//     const btn3 = document.createElement('button');
-
-//     const msgDiv = document.createElement('div');
-//     const scoreDiv = document.createElement('div');
-//     const announceDiv = document.createElement('div'); 
-
-//     const buttons = [btn1, btn2, btn3];
-
-//     // Update text
-//     btn1.textContent = 'Rock'; 
-//     btn2.textContent = 'Paper';
-//     btn3.textContent = 'Scissors';
-//     msgDiv.textContent = 'Message: The first player with 5 points wins the game'; 
-//     scoreDiv.textContent = `Running score: ${score}`; 
-//     announceDiv.style.cssText = 'color: rebeccapurple; font-weight: 700; font-size: 2em;'; 
-
-//     buttons.forEach(btn => {
-//         btn.addEventListener('click', (e) => {
-//             const playerSelection = e.target.textContent;
-//             const computerSelection = computerPlay(); 
-//             score += playRound(playerSelection, computerSelection);
-//             msgDiv.textContent = 'Message: ' + displayMessage(playerSelection, computerSelection);
-//             scoreDiv.textContent = `Running score: ${score}`; 
-//             announceDiv.textContent = ''; 
-//             if (Math.abs(score) === NUM_ROUNDS) { // check for winner
-//                 announceDiv.textContent = score > 0 ? "You win!" : "You lose!";
-//                 score = 0;
-//             }
-//         });
-//     })
-
-//     // Add them to the DOM
-//     document.body.appendChild(btn1);
-//     document.body.appendChild(btn2);
-//     document.body.appendChild(btn3);
-//     document.body.appendChild(msgDiv);
-//     document.body.appendChild(scoreDiv);
-//     document.body.appendChild(announceDiv);
-// }
+function reportWins() {
+    let playerWinsCount = winners.filter(winner => winner === "player").length;
+    let computerWinsCount = winners.filter(winner => winner === "computer").length;
+    let tiesCount = winners.filter(item => item === "tie").length;
+    console.log("Player wins: ", playerWinsCount);
+    console.log("Computer wins: ", computerWinsCount);
+    console.log("Ties: ", tiesCount);
+}
 
 game();
