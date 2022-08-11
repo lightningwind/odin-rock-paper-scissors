4e89b86218d288edad3f400b90ff70528fdb876f
David Islam <david.islam24@gmail.com>
2022-08-11 13:43:52 -0700

    Add comments and implement function displayRound()

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const choices = ["rock", "paper", "scissors"];
 const winners = [];
 const NUM_ROUNDS = 5;
 
+/* TODO: Resets the game */
 function resetGame() {
 
 }
 
+/* Starts the RPS game by attaching event listeners to
+the player's buttons. */
 function startGame() {
     const queryStr = ".playerBtns > button";
     const playerButtons = document.querySelectorAll(queryStr);
@@ -16,10 +19,10 @@ function startGame() {
     })
 }
 
-/* Plays one round of RPS if the game is not over. */
+/* Plays one round of RPS if the game is not already over. */
 function playRound(playerSelection) {
     const numWins = checkWins();
-    if (numWins >= 5) {
+    if (numWins >= NUM_ROUNDS) {
         return;
     }
     const computerSelection = computerChoice();
@@ -32,22 +35,36 @@ function playRound(playerSelection) {
 /* Displays what the player chose, computer chose, and who won
 the round. */
 function displayRound(playerSelection, computerSelection, winner) {
+    // Capitalize each word
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    winner = winner.charAt(0).toUpperCase() + winner.slice(1);
 
+    const winnerEle = document.querySelector(".winner");
+    const playerChoiceEle = document.querySelector(".playerChoice");
+    const computerChoiceEle = document.querySelector(".computerChoice");
+
+    winnerEle.textContent = winner === "Tie" ? "It was a tie!" : `The winner is: ${winner}`;
+    playerChoiceEle.textContent = `You chose: ${playerSelection}`;
+    computerChoiceEle.textContent = `The computer chose: ${computerSelection}`;
 }
 
 /* Updates tie, player, and computer counts. */
 function updateCounts() {
+    // Compute each count
     const playerWinsCount = winners.filter(winner => winner === "player").length;
     const computerWinsCount = winners.filter(winner => winner === "computer").length;
     const tiesCount = winners.filter(item => item === "tie").length;
 
-    const tiesScoreDiv = document.querySelector(".ties");
-    const playerScoreDiv = document.querySelector(".playerScore");
-    const computerScoreDiv = document.querySelector(".computerScore");
+    // Grab references to DOM nodes
+    const tiesScoreEle = document.querySelector(".ties");
+    const playerScoreEle = document.querySelector(".playerScore");
+    const computerScoreEle = document.querySelector(".computerScore");
 
-    tiesScoreDiv.textContent = `Ties: ${tiesCount}`;
-    playerScoreDiv.textContent = `Score: ${playerWinsCount}`;
-    computerScoreDiv.textContent = `Score: ${computerWinsCount}`;
+    // Update text content
+    tiesScoreEle.textContent = `Ties: ${tiesCount}`;
+    playerScoreEle.textContent = `Score: ${playerWinsCount}`;
+    computerScoreEle.textContent = `Score: ${computerWinsCount}`;
 }
 
 function computerChoice() {
@@ -56,6 +73,7 @@ function computerChoice() {
     return choices[idx];
 }
 
+/* Returns the winnner of this round. */
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "tie"; 
@@ -75,12 +93,6 @@ function checkWins() {
     const playerWinsCount = winners.filter(winner => winner === "player").length;
     const computerWinsCount = winners.filter(winner => winner === "computer").length;
     return Math.max(playerWinsCount, computerWinsCount);
-}
-
-function setWins() {
-    let playerWinsCount = winners.filter(winner => winner === "player").length;
-    let computerWinsCount = winners.filter(winner => winner === "computer").length;
-    let tiesCount = winners.filter(item => item === "tie").length;
 }
 
 startGame();
